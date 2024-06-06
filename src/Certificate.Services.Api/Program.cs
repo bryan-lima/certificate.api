b06c397ab970f8926d3ab2ff93d2d9a0962ad6f0
Bryan <bryanlima200@outlook.com>
2024-06-05 21:36:39 -0300

    Refactored the Program class

--- src/Certificate.Services.Api/Program.cs
+++ src/Certificate.Services.Api/Program.cs
@@ -1,25 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+WebApplicationBuilder _builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+_builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+_builder.Services.AddEndpointsApiExplorer();
+_builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication _app = _builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (_app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    _app.UseSwagger();
+    _app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+_app.UseHttpsRedirection();
 
-app.UseAuthorization();
+_app.UseAuthorization();
 
-app.MapControllers();
+_app.MapControllers();
 
-app.Run();
+_app.Run();
